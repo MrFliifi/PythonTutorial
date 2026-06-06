9bca6adf6c8eb58437b2c5a9fc66142fcabaf2e2
FehlBehr <felix.behrens@cgi.com>
2026-06-06 14:20:11 +0200

    Added chapter about tuples

--- 4_collections/4_2_lists.py
+++ 4_collections/4_2_lists.py
@@ -3,16 +3,16 @@
 # lists sind container. Es ist also eine Variable, die 0 bis n Werte beinhalten kann.
 # lists sind ordered und mutable.
 # lists erkennt man an folgendem Syntax:
-variablen_name = ["ein Wert", 2, True]
+list_name = ["ein Wert", 2, True]
 # Zuerst definiert man einen Namen. Darauf folgt der assignment operator =. Die list wird
 # dann mit [ geöffnet. Man kann dann beliebig viele Werte, mit beliebigen Datentypen 
 # eintragen. Jeder Wert muss durch ein , getrennt sein. Abschließen tut man die list mit ].
 
 # Da lists ordered sind, kann man die Werte der list mit dem Index abrufen.
 print("Das sind die Werte der list: ")
-print(variablen_name[0])
-print(variablen_name[1])
-print(variablen_name[2])
+print(list_name[0])
+print(list_name[1])
+print(list_name[2])
 
 # Da lists mutable sind, kann man die Werte der list verändern. Dafür gibt es mehrere functions:
 # list.append(x) # Hängt x ans Ende der list
---
+++ 4_collections/4_3_tuples.py
@@ -0,0 +1,34 @@
+# 4.3: Tuples
+
+# tuples sind container. Es ist also eine Variable, die 0 bis n Werte beinhalten kann.
+# tuples sind ordered und immutable.
+# tuples erkennt man an folgendem Syntax:
+
+tuple_name = ("ein Wert", 2, True)
+# Zuerst definiert man einen Namen. Darauf folgt der assignment operator =. Das tuple wird
+# dann mit () geöffnet. Man kann dann beliebig viele Werte, mit beliebigen Datentypen 
+# eintragen. Jeder Wert muss durch ein , getrennt sein. Abschließen tut man das tuple mit ).
+
+# Anders als bei lists, kann ein tuple nicht mehr verändert werden. tuples sind also konstant.
+# Muss man ein tuple aber dennoch verändern, kann man die Werte des tuples in eine list schreiben,
+# die list verändern und ein neues tuple mit den Werten der list erstellen.
+# Hier ein Beispiel dafür:
+
+new_list = list(tuple_name)  # Wir typecasten mit der list() function
+new_list.append("false")     # Wir fügen einen Wert mit der .append() function hinzu
+tuple_name = tuple(new_list) # Wir typecasten mit der tuple() function
+print("Hiermit haben wir den Wert von unserem Tuple überschrieben.")
+print(tuple_name)
+
+# Da tuples ordered sind, kann man die Werte des tuple mit dem Index abrufen.
+print("Das sind die Werte des tuples: ")
+print(tuple_name[0])
+print(tuple_name[1])
+print(tuple_name[2])
+
+# Da man die Werte mit einem Index abrufen kann, sind duplikate Werte möglich.
+
+# TODO:
+# 1. Erstelle ein tuple mit mindestens 5 Werten. Füge dem tuple einen neuen Wert hinzu.
+# 2. Entferne einen anderen Wert aus dem tuple. 
+# Gib nach jedem Schritt das tuple mit print() aus.
